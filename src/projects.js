bb84ab1463c742923e9fbac7f7c1717a849d0a7a
Christopher Giordano <chrisgcpg@gmail.com>
2024-08-07 17:59:19 -0400

    Added assets for resume, project, and about section.

--- src/projects.js
+++ src/projects.js
@@ -1,9 +1,21 @@
 const projects = [
     {
-        title: "Tech Blog",
-        description: "A web service for created and reading post made by other developers or tech savy people",
-        websiteLink: "https://tech-blog-snmx.onrender.com/",
-        name: "tech-blog"
+        title: "Portfolio v2",
+        description: "This is an updated portfolio I made using handlebars.",
+        websiteLink: "https://portfolio-v2-6ktr.onrender.com/",
+        name: "portfolio-v2"
+    },
+    {
+        title: "Weather Dashboard",
+        description: "This weather dashboard can display the weather for today and include a 5 day forecast.",
+        websiteLink: "https://milkytea6.github.io/weather-dashboard/",
+        name: "weather-dashboard"
+    },
+    {
+        title: "Netflix Movie Picker",
+        description: "This web application aims to simplify the daunting task of choosing a movie to watch on Netflix.",
+        websiteLink: "https://milkytea6.github.io/netflix-movie-picker/",
+        name: "netflix-movie-picker"
     },
     {
         title: "Task Board",
@@ -12,11 +24,18 @@ const projects = [
         name: "task-board"
     },
     {
-        title: "Weather Dashboard",
-        description: "This weather dashboard can display the weather for today and include a 5 day forecast. ",
-        websiteLink: "https://milkytea6.github.io/weather-dashboard/",
-        name: "weather-dashboard"
-    }
+        title: "Tech Blog",
+        description: "A web service for created and reading post made by other developers or tech savy people.",
+        websiteLink: "https://tech-blog-snmx.onrender.com/",
+        name: "tech-blog"
+    },
+    {
+        title: "Personal Blog",
+        description: "An application where users can create and view posts.",
+        websiteLink: "https://milkytea6.github.io/personal-blog/",
+        name: "personal-blog"
+    },
+
 ];
 // Exports the project data to App.jsx
 export default projects;
